de5906a5f6b62855566bce77e5f494dbc225a239
Daniel Copeland <copelandic24@gmail.com>
2013-02-23 18:17:27 +0000

    new repository

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+require.config({
+  shim: {
+  	"backbone": {
+  		deps: ['../components/underscore/underscore', 'jquery'],
+  		exports: 'Backbone'
+  	}
+  },
+
+  paths: {
+    jquery: 'vendor/jquery.min',
+    backbone: '../components/backbone/backbone'
+  }
+});
+ 
+require(['backbone'], function(Backbone) {
+	console.log( Backbone );
+});
