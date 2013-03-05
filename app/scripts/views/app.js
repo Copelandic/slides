ce60ede055a12140bf5107d02d6540e8639ff600
Daniel Copeland <daniel@rehabstudio.com>
2013-03-05 17:12:50 +0000

    Add functionality to change slides

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -19,6 +19,20 @@ function(Backbone, SlidesView, SlidesCollection, MainRouter) {
 
 			App.router = new MainRouter();
 			Backbone.history.start();
+		},
+
+		events: {
+			'keyup': 'keyup'
+		},
+
+		keyup: function(e) {
+			// 37 = left
+			// 39 = right
+			if ( e.keyCode === 37 || e.keyCode === 39) {
+				App.Vent.trigger('changeSlide', {
+					direction: e.keyCode === 39 ? 'next' : 'prev'
+				});
+			}
 		}
 	});
 
--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -3,7 +3,70 @@ define(['backbone', 'views/slide'], function(Backbone, SlideView) {
 		el: $('.slides'),
 
 		initialize: function() {
+			this.currentSlideIndex = 1;
+			this.numSlides = this.collection.length;
+			this.transitionSpeed = 400;
+
 			this.renderAll();
+
+			App.Vent.on('init', this.hideAllButFirst, this);
+			App.Vent.on('changeSlide', this.changeSlide, this);
+		},
+
+		hideAllButFirst: function() {
+			this.$el.children(':nth-child(n+2)').hide();
+		},
+
+		changeSlide: function(opts) {
+			var newSlide;
+			var slides = this.$el.children();
+			var self = this;
+
+			this.setCurrentSlideIndex(opts);
+			newSlide = this.getNextSlide(slides);
+			this.animateToNewSlide(slides, newSlide, opts.direction);
+
+			App.router.navigate('/slides/' + this.currentSlideIndex);
+		},
+
+		setCurrentSlideIndex: function(opts) {
+
+			if ( opts.slideIndex ) {
+				return this.currentSlideIndex = ~~opts.slideIndex
+			}
+
+			this.currentSlideIndex += opts.direction === 'next' ? 1 : -1;
+
+			if ( this.currentSlideIndex > this.numSlides ) {
+				this.currentSlideIndex = 1;
+			}
+
+			if ( this.currentSlideIndex <= 0 ) {
+				this.currentSlideIndex = this.numSlides;
+			}
+		},
+
+		getNextSlide: function(slides) {
+			return slides.eq(this.currentSlideIndex - 1);
+		},
+
+		animateToNewSlide: function(slides, newSlide, direction) {
+			slides.filter(':visible')
+				.css('position', 'absolute')
+				.animate({
+					top: direction === 'next' ? '100%' : '-100%',
+					opacity: 'hide'
+
+				}, this.transitionSpeed, function() {
+					$(this).css('top', 0);
+
+					newSlide
+						.css('top', direction === 'next' ? '-100%' : '100%')
+						.animate({
+							top: 0,
+							opacity: 'show'
+						}, self.transitionSpeed);
+				});
 		},
 
 		renderAll: function() {
