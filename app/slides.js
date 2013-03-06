c1c22298e566b7872e2a89a99754acdc193ba5da
Daniel Copeland <daniel@rehabstudio.com>
2013-03-06 10:36:09 +0000

    slides.js added

---
+++ app/slides.js
@@ -0,0 +1,16 @@
+window.slides = [
+	{ title: 'Wee Slideshow' },
+	{
+		title: 'Some CSS Code Here',
+		snippet: 'snippets/ex1.css'
+	},
+	{
+		quote: 'Knowledge is power',
+		cite: 'Sir Francis Bacon'
+	},
+	{ 
+		title: 'Why This Matters',
+		bullets: ['Point One', 'Point Two', 'Point Three'] 
+	},
+	{ image: 'http://fillmurray.com/g/200/300' }
+];
